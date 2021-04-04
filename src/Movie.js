28ad03c29591b1a2d66951334a2bec7d3f4ea69e
aminiyolo <73641636+aminiyolo@users.noreply.github.com>
2021-04-04 13:57:55 +0900

    React - movie css

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({ key, id, title, year, genres, summary, poster}) {
-  return(
-    <div>
-      <h4>{title}</h4>
-      <h4>{year}</h4>
-      <h4>[{genres}]</h4>
-      <h4>{summary}</h4>
-      <img src={poster}></img>
+function Movie({ title, year, genres, summary, poster}) {
+    return(
+    <div className="movie">
+        <img className="movie__img" src={poster} alt={title}></img>
+      <div className="movie__info">
+        <h3 className="movie__title">Title: {title}</h3>
+        <h4 className="movie__year">Release Year: {year}</h4>
+        <ul className="movie__genres">
+          {genres.map( (genre, index) => (
+            <li key={index} className="genre">{genre}</li>))}
+        </ul>
+            <p className="movie__summary">Summary: {summary.slice(0, 150)}...</p>
+      </div>
     </div>
   )
 }
@@ -17,7 +23,7 @@ Movie.propTypes = {
   id: propTypes.number.isRequired,
   title: propTypes.string.isRequired,
   year: propTypes.number.isRequired,
-  genres: propTypes.array.isRequired,
+  genres: propTypes.arrayOf(propTypes.string).isRequired,
   summary: propTypes.string.isRequired,
   poster: propTypes.string.isRequired,  
 }
